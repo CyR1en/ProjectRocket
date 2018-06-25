f8e38c3bb41bd16e081b3a474da44fe449255a98
CyR1en <cyrien3519@gmail.com>
2018-06-25 02:31:08 -0600

    1.1

--- RocketBot/src/main/java/com/rocket/rocketbot/listeners/SynchronizeListener.java
+++ RocketBot/src/main/java/com/rocket/rocketbot/listeners/SynchronizeListener.java
@@ -2,45 +2,70 @@ package com.rocket.rocketbot.listeners;
 
 import com.rocket.rocketbot.RocketBot;
 import com.rocket.rocketbot.events.SynchronizeEvent;
-import me.lucko.luckperms.LuckPerms;
-import me.lucko.luckperms.api.LuckPermsApi;
-import me.lucko.luckperms.api.User;
+import net.dv8tion.jda.core.entities.Guild;
 import net.dv8tion.jda.core.entities.Member;
+import net.dv8tion.jda.core.entities.Role;
 import net.dv8tion.jda.core.managers.GuildController;
 import net.md_5.bungee.api.config.ServerInfo;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
+import net.md_5.bungee.api.event.PluginMessageEvent;
 import net.md_5.bungee.event.EventHandler;
 
-import java.io.ByteArrayOutputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
+import java.io.*;
 
 public class SynchronizeListener extends SListener {
+
+    private SynchronizeEvent evt;
 
     public SynchronizeListener(RocketBot rocketBot) {
         super(rocketBot);
     }
 
     @EventHandler
-    public void sendPluginMessage(SynchronizeEvent event) {
+    public void handleSynchronize(SynchronizeEvent event) {
         ServerInfo server = event.getProxiedPlayer().getServer().getInfo();
-        sendToBukkit("command", event.getProxiedPlayer().getName(), server);
+        sendToBukkit("GQuery", event.getProxiedPlayer().getUniqueId().toString(), server);
+        evt = event;
     }
 
     @EventHandler
-    public void handleSynchronize(SynchronizeEvent event) {
-        ProxiedPlayer pp = event.getProxiedPlayer();
-        LuckPermsApi api = LuckPerms.getApi();
-        User user = api.getUser(event.getProxiedPlayer().getUniqueId());
-        String group = user != null ? user.getPrimaryGroup() : null;
+    public void getGroupQuery(PluginMessageEvent event) {
+        if (event.getTag().equalsIgnoreCase("BungeeCord")) {
+            DataInputStream in = new DataInputStream(new ByteArrayInputStream(event.getData()));
+            try {
+                String channel = in.readUTF();
+                if(channel.equals("RQuery")){
+                    ServerInfo server = evt.getProxiedPlayer().getServer().getInfo();
+                    String group = in.readUTF();
+                    handleRole(group);
+                    sendToBukkit("command", "", server);
+                }
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
+        }
+    }
+
+    private void handleRole(String group) {
+        ProxiedPlayer pp = evt.getProxiedPlayer();
         if(group != null) {
             getRocketBot().getBot().getJda().getGuilds().forEach(g -> {
-                Member m = g.getMemberById(event.getUnifiedUser().getDUser().getUser().getId());
+                Member m = g.getMemberById(evt.getUnifiedUser().getDUser().getUser().getId());
                 GuildController controller = g.getController();
-                controller.createRole().setName(group).queue(r -> controller.addSingleRoleToMember(m, r).queue());
-                controller.setNickname(m, pp.getDisplayName()).queue();
+                if(roleExists(g, group))
+                    g.getRolesByName(group, true).forEach(r -> controller.addSingleRoleToMember(m, r).queue());
+                else
+                    controller.createRole().setName(group).queue(r -> controller.addSingleRoleToMember(m, r).queue());
+                controller.setNickname(m, pp.getName()).queue();
             });
         }
+    }
+
+    private boolean roleExists(Guild g, String role) {
+        for (Role r : g.getRoles())
+            if(r.getName().equalsIgnoreCase(role))
+                return true;
+        return false;
     }
 
     private void sendToBukkit(String channel, String message, ServerInfo server) {
@@ -52,8 +77,7 @@ public class SynchronizeListener extends SListener {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        // Note the "Return". It is the channel name that we registered in our Main class of Bungee plugin.
+        
         server.sendData("Return", stream.toByteArray());
-
     }
 }
--- RocketClient/src/main/java/com/rocket/rocketclient/listeners/PluginChannelListener.java
+++ RocketClient/src/main/java/com/rocket/rocketclient/listeners/PluginChannelListener.java
@@ -1,12 +1,15 @@
 package com.rocket.rocketclient.listeners;
 
 import com.rocket.rocketclient.RocketClient;
+import me.lucko.luckperms.api.LuckPermsApi;
+import me.lucko.luckperms.api.User;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
+import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.messaging.PluginMessageListener;
 
-import java.io.ByteArrayInputStream;
-import java.io.DataInputStream;
+import java.io.*;
+import java.util.UUID;
 
 public class PluginChannelListener implements PluginMessageListener {
 
@@ -25,8 +28,34 @@ public class PluginChannelListener implements PluginMessageListener {
                 String command = rocketClient.getRConfigManager().getGeneralConfig().getCommand();
                 Bukkit.getServer().dispatchCommand(Bukkit.getServer().getConsoleSender(), command);
             }
+            if(sub.equals("GQuery")) {
+                RegisteredServiceProvider<LuckPermsApi> provider = Bukkit.getServicesManager().getRegistration(LuckPermsApi.class);
+                if (provider != null) {
+                    LuckPermsApi api = provider.getProvider();
+                    String sUUID = in.readUTF();
+                    User user = api.getUser(UUID.fromString(sUUID));
+                    if(user != null)
+                        sendToBungeeCord(player, "RQuery", user.getPrimaryGroup());
+                }
+            }
         } catch (Exception ex) {
             ex.printStackTrace();
         }
     }
+
+    public void sendToBungeeCord(Player p, String channel, String sub, String... other){
+        ByteArrayOutputStream b = new ByteArrayOutputStream();
+        DataOutputStream out = new DataOutputStream(b);
+        try {
+            out.writeUTF(channel);
+            out.writeUTF(sub);
+            for (String o : other) {
+                out.writeUTF(o);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        p.sendPluginMessage(RocketClient.getPlugin(RocketClient.class), "BungeeCord", b.toByteArray());
+    }
+
 }
